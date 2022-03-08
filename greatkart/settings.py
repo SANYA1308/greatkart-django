572754d0f74e79f95407850a65c60350d009a66a
Sanya <sanyagondi1308@gmail.com>
2022-03-08 22:46:42 +0530

    store and carts functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'category.apps.CategoryConfig',
     'accounts.apps.AccountsConfig',
     'store.apps.StoreConfig',
+    'carts.apps.CartsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
                 
             ],
         },
